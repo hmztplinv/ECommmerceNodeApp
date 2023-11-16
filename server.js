1aedf10123cc67a28f0ebdc1aaa62d9746ba23e7
hmztplinv <hmz.tpal@gmail.com>
2023-11-16 16:59:19 +0300

    server.js added

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('welcome server.js');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port=process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the server' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
